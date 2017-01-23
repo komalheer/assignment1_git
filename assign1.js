0bfecbed6a1880d6f998aecb306f0d9b80857b66
komal <komalheer1992@gmail.com>
2017-01-23 02:20:19 +0000

    first commit

---
+++ assign1.js
@@ -0,0 +1,9 @@
+let arr1=[]
+arr1.push(2)
+console.log(`arr1 after push(2):${arr1}`)
+arr1.unshift(1)
+console.log(`arr1 after unshift(1):${arr1}`)
+arr1.push(3)
+arr1.pop()
+arr1.shift()
+console.log(`New array:${arr1}`)
